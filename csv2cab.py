90e629925ca677e96dd0ebacaa8a93315e7336b2
Michael Heitmann, N0SO <mheitmann@n0so.net>
2019-09-01 23:49:41 -0500

    Made csv2cab a child class of CabrilloUtils

--- csv2cab.py
+++ csv2cab.py
@@ -30,6 +30,9 @@ csv2cab - Process a .csv file from MSExcel (or other spreadsheets)
           V1.0.2 - 2018-05-01 
           Made csv2cab callable from another module. The stand-alone
           command line functions still work too.
+	  
+	  V1.0.3 - 2019-09-01
+	  Making this a child class of Cabrilloutils.
           
 """
 import datetime
@@ -37,13 +40,13 @@ import argparse
 import sys
 import string
 from os.path import basename
-from CabrilloUtils import *
+from CabrilloUtils import CabrilloUtils
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 TAGLINE = 'SOAPBOX: This file processed by the csv2cab utility.\n'
 ARGS = None
 
-class csv2CAB():
+class csv2CAB(CabrilloUtils):
    def __init__(self, csvfilename = None):
       if (csvfilename):
          self.main(csvfilename)
@@ -51,34 +54,32 @@ class csv2CAB():
    def readcsvcabFile(self, csvfileName):
       """Read CSV file and return data. If this file is
          not a CABRILLO format file, return None"""
-      cab = CabrilloUtils()
-      csvdata = cab.readFile(csvfileName)
+      csvdata = self.readFile(csvfileName)
       
       if (csvdata):
-         if (cab.IsThisACabFile(csvdata) == False):
+         if (self.IsThisACabFile(csvdata) == False):
             csvdata = None
       
       return csvdata
       
       
-   def processLine(self, cab, line):
+   def processLine(self, line):
       newline = None
-      for tag in cab.CABRILLOTAGS:                 #Skip any line without a CAB tab
+      for tag in self.CABRILLOTAGS:                 #Skip any line without a CAB tab
          if (tag in line):
             newline = r""
-            newline = cab.packLine(line)
+            newline = self.packLine(line)
             if newline == 'QSO:':                  #Skip blank QSO: lines
                newline = None
             break
       return newline
          
    def processcsvData(self, csvdata):
-      cab = CabrilloUtils()
       tag_needed = True
       cabdata = r""
       
       for line in csvdata:
-         newline = self.processLine(cab, line)
+         newline = self.processLine(line)
          if (newline):                              #Skip invalid lines
             if (tag_needed):                      
                if ('SOAPBOX:' in newline):          #Flag this file as processed by me
@@ -90,12 +91,11 @@ class csv2CAB():
    def main(self, csvfilename):
       cabdata = r""
       csvdata = self.readcsvcabFile(csvfilename)
-
+ 
       if (csvdata == None):
          print('File %s is not a Cabrillo Format File.'%(csvfilename))
       else:
          cabdata = self.processcsvData(csvdata)
-      
          cabfile = basename(csvfilename) + ".log"
          with open(cabfile,'w') as f:
             f.write(cabdata)
@@ -109,8 +109,15 @@ class get_args():
             self.args = self.getargs()
             
     def getargs(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-v', '--version', action='version', version = VERSION)
+        parser = argparse.ArgumentParser(description = \
+	   'Version '+VERSION + '-- \n'+ \
+	   'Process a .csv file from MSExcel (or other ' +\
+	   'spreadsheets) and make it a .CAB file by '+ \
+	   'removing all commas, blank QSO: lines, etc. ' +\
+	   'Usually used to clean a file submitted using ' +\
+	   'AA0CL\'s MOQP_log.xls form.')
+        parser.add_argument('-v', '--version', action='version', 
+	                    version = VERSION)
         parser.add_argument("-i", "--inputfile", default=None,
             help="Specifies the log file input file name")
         return parser.parse_args()
@@ -121,16 +128,7 @@ class theAPP():
 
    def main(self):
       args = get_args()
-      csv2CAB(args.args.inputfile)
-# The remainder of these were for unit testing the callable functions.      
-#      mycab = csv2CAB()
-#      csvdata = mycab.readcsvcabFile(args.args.inputfile)
-#      if (csvdata):
-#         cabdata = mycab.processcsvData(csvdata)
-#         print csvdata
-#         print cabdata
-#      mycab.main(args.args.inputfile)      
-      
+      csv2CAB(args.args.inputfile.strip())
 
 if __name__ == '__main__':
    app=theAPP()
