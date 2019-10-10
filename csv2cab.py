e4ff2b544b6b5e73039bc1b7ae524b5f91a74c06
N0SO <mheitmann@n0so.net>
2019-10-10 15:29:40 -0500

    Changes to csv2cab
    Combining GUI and command line
    py3 updates

--- csv2cab.py
+++ csv2cab.py
@@ -1,46 +1,50 @@
 #!/usr/bin/python
 """
-csv2cab - Process a .csv file from MSExcel (or other spreadsheets)
-          and make it a .CAB file by removing all commas, blank
-          QSO: lines, etc. Usually used to clean a file submitted
-          using AA0CL's MOQP_log.xls form.
-          
-          Usage:
-             python csv2cab.py -i logfilename.csv
-             
-          This will create a file named logfile.csv.log that should
-          be in the same folder as the source.
-          
-          Starting with V1.0.2, can also be called from inside your 
-	  code:
-	    from csv2cab import *
-	    mycab = csv2cab('csvcabfilename')
-	               
-	    --or-- if you want to manipulate the cab data:
-	    mycab = csv2cab()
-	    mycsvdata = mycab.readcsvcabFile(csvcabfilename)
-	    mycabdata = mycab.processcsvData(mycsvdata)
+csv2cab - 
+Process a .csv file from MSExcel (or other spreadsheets)
+and make it a .CAB file by removing all commas, blank
+QSO: lines, etc. Usually used to clean a file submitted
+using AA0CL's MOQP_log.xls form.
 
-          
-          This program inserts whatever is in the TAGLINE constant
-          as the first line of the SOAPBOX: fields
-          
-          V1.0.1 - Initial release
-          
-          V1.0.2 - 2018-05-01 
-          Made csv2cab callable from another module. The stand-alone
-          command line functions still work too.
-	  
-	  V1.0.3 - 2019-09-01
-	  Making this a child class of Cabrilloutils.
-          
+Usage:
+ python csv2cab.py -i logfilename.csv
+
+This will create a file named logfile.csv.log that should
+be in the same folder as the source.
+
+Starting with V1.0.2, can also be called from inside your 
+code:
+from csv2cab import *
+mycab = csv2cab('csvcabfilename')
+
+--or-- if you want to manipulate the cab data:
+mycab = csv2cab()
+mycsvdata = mycab.readcsvcabFile(csvcabfilename)
+mycabdata = mycab.processcsvData(mycsvdata)
+
+
+This program inserts whatever is in the TAGLINE constant
+as the first line of the SOAPBOX: fields
+Update History:
+* Mon Apr 30 2019 Mike Heitmann, N0SO <n0so@arrl.net>
+- V1.0.1 - Initial release
+* Tue May 01 2019 Mike Heitmann, N0SO <n0so@arrl.net>
+- V1.0.2 - 2018-05-01 
+- Made csv2cab callable from another module. The stand-alone
+- command line functions still work too.
+* Thu Oct 10 2019 Mike Heitmann, N0SO <n0so@arrl.net>
+- V1.0.3 - 2019-10-10
+- Made csv2cab inherit from CabrilloUtils.
+- Make module CSVtoCAB with classes csv2CAB and ui.
+- Added csv2cab script that will do both command line and GUI.
+- Fixed a bug in processcsvData that omitted the END-OF-LOG: tag.
 """
 import datetime
 import argparse
 import sys
 import string
 from os.path import basename
-from CabrilloUtils import CabrilloUtils
+from CabrilloUtils import *
 
 VERSION = '1.0.3'
 TAGLINE = 'SOAPBOX: This file processed by the csv2cab utility.\n'
@@ -51,9 +55,14 @@ class csv2CAB(CabrilloUtils):
       if (csvfilename):
          self.main(csvfilename)
          
+   def __version__(self):
+        return VERSION
+
+         
    def readcsvcabFile(self, csvfileName):
       """Read CSV file and return data. If this file is
          not a CABRILLO format file, return None"""
+      #cab = CabrilloUtils()
       csvdata = self.readFile(csvfileName)
       
       if (csvdata):
@@ -75,6 +84,7 @@ class csv2CAB(CabrilloUtils):
       return newline
          
    def processcsvData(self, csvdata):
+      #cab = CabrilloUtils()
       tag_needed = True
       cabdata = r""
       
@@ -91,11 +101,12 @@ class csv2CAB(CabrilloUtils):
    def main(self, csvfilename):
       cabdata = r""
       csvdata = self.readcsvcabFile(csvfilename)
- 
+
       if (csvdata == None):
          print('File %s is not a Cabrillo Format File.'%(csvfilename))
       else:
          cabdata = self.processcsvData(csvdata)
+      
          cabfile = basename(csvfilename) + ".log"
          with open(cabfile,'w') as f:
             f.write(cabdata)
@@ -103,32 +114,9 @@ class csv2CAB(CabrilloUtils):
       return cabdata
 
       
-class get_args():
-    def __init__(self):
-        if __name__ == '__main__':
-            self.args = self.getargs()
-            
-    def getargs(self):
-        parser = argparse.ArgumentParser(description = \
-	   'Version '+VERSION + '-- \n'+ \
-	   'Process a .csv file from MSExcel (or other ' +\
-	   'spreadsheets) and make it a .CAB file by '+ \
-	   'removing all commas, blank QSO: lines, etc. ' +\
-	   'Usually used to clean a file submitted using ' +\
-	   'AA0CL\'s MOQP_log.xls form.')
-        parser.add_argument('-v', '--version', action='version', 
-	                    version = VERSION)
-        parser.add_argument("-i", "--inputfile", default=None,
-            help="Specifies the log file input file name")
-        return parser.parse_args()
-
-class theAPP():
-   def __init__(self):
-      self.main()
-
-   def main(self):
-      args = get_args()
-      csv2CAB(args.args.inputfile.strip())
+     
 
 if __name__ == '__main__':
-   app=theAPP()
+   app = csv2CAB()
+   print ('Classname: %s Version: %s'%(app.__class__.__name__,
+                                       app.__version__()))
