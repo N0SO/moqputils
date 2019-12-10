32d817f8a3b7ac7e412c2adb3c189a5adef18a62
N0SO <mheitmann@n0so.net>
2019-12-10 15:49:37 -0600

    First working version of SHOWME

--- moqputils/showmeaward.py
+++ moqputils/showmeaward.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python
-"""
-ShowMe  - Determine which Missouri QSO Party stations 
-          qualify for the ShowMe and/or MISSOURI awards
 
-Update History:
-* Thu Dec 09 2019 Mike Heitmann, N0SO <n0so@arrl.net>
-- V0.0.1 - Start of work
-"""
+import os
+from moqpcategory import *
 
 COMMONCALLS = ['K0M', 'N0M','W0M',
                'K0O', 'N0O','W0O',
@@ -16,66 +11,182 @@ SHOWMECALLS = ['K0H', 'N0H','W0H',
                'K0W', 'N0W','W0W',
                'K0E', 'N0E','W0E']
                 
-MISSOURICALLS = ['K0I', 'N0I','W0I',
+MOCALLS = ['K0I', 'N0I','W0I',
                  'K0U', 'N0U','W0U',
                  'K0R', 'N0R','W0R']
-
+                 
+WILDCARDS = ['W0MA']
 
 SHOWMEKEYS = 'SHOWME'
 
 MOKEYS = 'MISSOURI'
 
-class ShowMeAward():
-    award = dict()
-    award = {'S':None,
-    'H':None,
-    'O':None,
-    'W':None,
-    'M':None,
-    'E':None}
+VERSION = '0.1.0'
 
-    def __init__(self, filename=None):
-        if (filename):
-           if (filename):
-              self.appMain(filename)
+"""
+GenAward - Generic award class
+    Generally not called by itself, but is used as a
+    code base for other awards like SHOWME and MISSOURI.
+    Those classes would inherit this class.
+"""
+class GenAward():
 
+    def __init__(self):
+        pass
+        
+    """
+    Initializes and returns an award object with KEYS
+    """
+    def init_award(self, KEYS):
+        award = dict()
+        for key in KEYS:
+            award[key] = None
+        return award
+
+    """
+    Checks the award object KEYS for all elements set.
+    Returns True (BINGO!) if all elements are set
+    """
     def _bingo_(self, award, KEYS):
         bingo = True
+        retval = False
         for key in KEYS:
-            if (award[key] == None):
-               bingo = False
-               break
-        return bingo
-    
-    def _checkcall_(self, call):
+            if key in award:
+                if (award[key] == None):
+                   bingo = False
+                   break
+            else:
+                break
+        retval = bingo
+        return retval
+
+    """
+    Checks the call passed in to see if the award object 
+    has that key set. Returns True if needed.
+    """
+    def _checkcall_(self, call, callset, award):
+       retval = None
        #print(call)
-       if ( (call in COMMONCALLS) or \
-            (call in SHOWMECALLS) ):
+       if (call in callset):
           key = call[2] 
           #print('call = %s, key = %s'%(call, key))
-          if (key in self.award):
+          if (key in award):
               #print('Found %s for key %s'%(call, key))
-              if (self.award[key] == None):
-                  self.award[key] = call
+              if (award[key] == None):
+                  retval = key
+       return retval
+       
+    def combineLists(self, list1, list2):
+       retlist = []
+       for i in list1:
+           retlist.append(i)
+       for i in list2:
+           retlist.append(i)
+       return retlist
 
     def checkLog(self, logqsos):
-       showme = False
+       retval = False
        for qso in logqsos:
-          self._checkcall_(qso['URCALL'])
-          if (self._bingo_(self.award, SHOWMEKEYS)):
-              print('***BINGO*** - SHOWME complete!')
-              Showme = True
-              print(award)
+          #print(qso)
+          key = self._checkcall_(qso['URCALL'], 
+                                 self.callset, 
+                                 self.Award)
+          if (key):
+              self.Award[key] = qso['URCALL']
+              
+          if (self._bingo_(self.Award, self.KEYS)):
+              #print('***BINGO*** - SHOWME complete!')
+              retval = True
+              #print(award)
               break
           else:
-              print('Not Bingo!')
+              #print('Not Bingo!')
+              continue
           #print(qso)
           #print(self.award)
-       return showme
+       return retval
        
+"""
+ShowMeAward - Child class of GenAward class
+              taylored for the 2019 MOQP SHOWME Award.
+"""
+class ShowMeAward(GenAward):
+
+    def __init__(self, qsolist=None):
+       self.KEYS = SHOWMEKEYS
+       self.Award = self.init_award(SHOWMEKEYS)
+       self.callset = self.combineLists(SHOWMECALLS, COMMONCALLS)
+#       print('Keys = %s\nAward = %s\ncallset = %s'%(self.KEYS,
+#                                                   self.Award, 
+#                                                   self.callset))
+       if (qsolist):
+              self.appMain(qsolist)
+       
+    def appMain(self, qsolist):
+        Bingo = self.checkLog(qsolist)
+        #print('SHOWME AWARD = %s\nStats: %s'%(Bingo, 
+        #                                     self.Award))
+        return [Bingo,self.Award]
+
+"""
+MissouriAward - Child class of GenAward class
+              taylored for the 2019 MOQP MISSOURI Award.
+"""
+class MissouriAward(GenAward):
+
+    def __init__(self, qsolist=None):
+       self.KEYS = MOKEYS
+       self.Award = self.init_award(self.KEYS)
+       self.callset = self.combineLists(MOCALLS, COMMONCALLS)
+#       print('Keys = %s\nAward = %s\ncallset = %s'%(self.KEYS,
+#                                                   self.Award, 
+#                                                   self.callset))
+       if (qsolist):
+              self.appMain(qsolist)
+       
+    def appMain(self, qsolist):
+        Bingo = self.checkLog(qsolist)
+#        print('MISSOURI AWARD = %s\nStats: %s'%(Bingo, 
+#                                                  self.Award))
+        return [Bingo,self.Award]
+
+"""
+ShowMe - For command line operations. 
+"""
+class ShowMe(MOQPCategory):
+
+    def __init__(self, pathname = None):
+        if (pathname):
+            self.appMain(pathname)
+            
+    def scoreFile(self, pathname):
+        log = self.parseLog(pathname)
+        if log:
+            if (log['ERRORS'] == []):
+#                award = ShowMeAward(log['QSOLIST'])
+#                awardm = MissouriAward(log['QSOLIST'])
+                award = ShowMeAward()
+                awardm = MissouriAward()
+                print(award.appMain(log['QSOLIST']))
+                print(awardm.appMain(log['QSOLIST']))
+            else:
+                print('log file %s has errors:\n %s'\
+				%(pathname, log['ERRORS']))
+
+    def scorefileList(self, pathname):
+        for (dirName, subdirList, fileList) in os.walk(pathname, topdown=True):
+           if (fileList != ''): 
+               Headers = True
+               for fileName in fileList:
+                   fullPath = ('%s/%s'%(dirName, fileName))
+                   self.scoreFile(fullPath)
+
     def appMain(self, pathname):
-        print(self.checkLog(pathname))
-        print(self.award)      
+        print('Input path: %s'%(pathname))
+        if (os.path.isfile(pathname)):
+            self.scoreFile(pathname)
+        else:
+            self.scorefileList(pathname)
 
 if __name__ == '__main__':
    app = ShowMeAward()
