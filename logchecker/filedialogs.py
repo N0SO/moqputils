29f9883a5d14323368115befd108e9e306975a19
n0so <mheitmann@n0so.net>
2021-02-05 11:38:38 -0600

    Added sample output display - needs more work

---
+++ logchecker/filedialogs.py
@@ -0,0 +1,75 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk
+
+class my_file_open(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Select MOQP Log File to Check")
+        self.fileName=None
+        
+    def on_file_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OPEN,
+            Gtk.ResponseType.OK,
+        )
+
+        self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+            self.fileName = dialog.get_filename()
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def on_folder_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a folder",
+            parent=self,
+            action=Gtk.FileChooserAction.SELECT_FOLDER,
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+        )
+        dialog.set_default_size(800, 400)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Select clicked")
+            print("Folder selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def add_filters(self, dialog):
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("CAB files")
+        filter_text.add_pattern("*.LOG")
+        dialog.add_filter(filter_text)
+
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("CSV files")
+        filter_text.add_pattern("*.CSV")
+        dialog.add_filter(filter_text)
+
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
--- logchecker/ui.py
+++ logchecker/ui.py
@@ -4,6 +4,9 @@ import gi, os, sys
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+from logchecker.treesample import TreeViewFilterWindow
+from logchecker.filedialogs import my_file_open
+
 VERSION = '0.0.1'
 
 class About1():
@@ -29,75 +32,6 @@ class About1():
         about.set_website("http://n0so.net")
         about.run()
         about.destroy()
-
-class my_file_open(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Select MOQP Log File to Check")
-        self.fileName=None
-        
-    def on_file_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OPEN,
-            Gtk.ResponseType.OK,
-        )
-
-        self.add_filters(dialog)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Open clicked")
-            print("File selected: " + dialog.get_filename())
-            self.fileName = dialog.get_filename()
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-    def add_filters(self, dialog):
-        filter_text = Gtk.FileFilter()
-        filter_text.set_name("CAB files")
-        filter_text.add_pattern("*.LOG")
-        dialog.add_filter(filter_text)
-
-        filter_text = Gtk.FileFilter()
-        filter_text.set_name("CSV files")
-        filter_text.add_pattern("*.CSV")
-        dialog.add_filter(filter_text)
-
-        filter_text = Gtk.FileFilter()
-        filter_text.set_name("Text files")
-        filter_text.add_mime_type("text/plain")
-        dialog.add_filter(filter_text)
-
-        filter_any = Gtk.FileFilter()
-        filter_any.set_name("Any files")
-        filter_any.add_pattern("*")
-        dialog.add_filter(filter_any)
-
-    def on_folder_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a folder",
-            parent=self,
-            action=Gtk.FileChooserAction.SELECT_FOLDER,
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
-        dialog.set_default_size(800, 400)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Select clicked")
-            print("Folder selected: " + dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
 
 class runLogCheck():
 
@@ -125,6 +59,9 @@ class runLogCheck():
          
     def showData(self, Data_to_Show):
         print(Data_to_Show)
+        lines = Data_to_Show.splitlines()
+        llines = lines[1].split('\t')
+        show = TreeViewFilterWindow([llines])
 
 class Handler():
 
