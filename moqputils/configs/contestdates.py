8460c02db36bb0169056f8b83b3494479f470cbc
Mike Heitmann, N0SO <n0so@arrl.net>
2021-08-10 21:01:53 -0500

    Adding contest dates for 2021

--- moqputils/configs/contestdates.py
+++ moqputils/configs/contestdates.py
@@ -4,8 +4,8 @@
      contest start/stop times.
 """
 from datetime import datetime
-DAY1START = datetime.strptime('2020-04-04 14:00', '%Y-%m-%d %H:%M')
-DAY1STOP = datetime.strptime('2020-04-05 04:00', '%Y-%m-%d %H:%M')
-DAY2START = datetime.strptime('2020-04-05 14:00', '%Y-%m-%d %H:%M')
-DAY2STOP = datetime.strptime('2020-04-05 20:00', '%Y-%m-%d %H:%M')
+DAY1START = datetime.strptime('2021-07-31 14:00', '%Y-%m-%d %H:%M')
+DAY1STOP = datetime.strptime('2021-08-01 04:00', '%Y-%m-%d %H:%M')
+DAY2START = datetime.strptime('2021-08-01 14:00', '%Y-%m-%d %H:%M')
+DAY2STOP = datetime.strptime('2021-08-01 20:00', '%Y-%m-%d %H:%M')
 
