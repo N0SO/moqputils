5da18cf2b869b612a8ee60a25a37399fa768bb0f
n0so <n0so@arrl.net>
2021-08-30 23:45:03 -0500

    Added mqpdevpath.py to automatically add the development path.
    On branch development
    Changes to be committed:
    	modified:   bin/mqpcategory
    	new file:   bin/mqpdevpath.py
    	modified:   bin/mqplogcheck
    	modified:   bin/qsocheck

---
+++ bin/mqpdevpath.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+If the development module source paths exist, 
+add them to the python path
+"""
+
+import os.path
+import sys
+
+whereami = os.path.split( os.path.realpath(__file__) )
+pathsplit = os.path.split(whereami[0])
+#print("here I am :", whereami, pathsplit)
+
+DEVMODPATH = [pathsplit[0]]
+
+for mypath in DEVMODPATH:
+        if ( os.path.exists(mypath) and \
+          (os.path.isfile(mypath) == False) ):
+            sys.path.insert(0, mypath)
