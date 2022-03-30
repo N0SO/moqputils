c6a02ba7a92f81a63918454d3737e32686a9943b
n0so <n0so@arrl.net>
2022-03-29 20:52:17 -0500

    Updating contest dates and database names for 2022

--- moqputils/configs/contestdates.py
+++ moqputils/configs/contestdates.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """ Defines contest period in UTC. 
      This will be imported by QSOUtils to set 
      contest start/stop times.
 """
 from datetime import datetime
-DAY1START = datetime.strptime('2021-07-31 14:00', '%Y-%m-%d %H:%M')
-DAY1STOP = datetime.strptime('2021-08-01 04:00', '%Y-%m-%d %H:%M')
-DAY2START = datetime.strptime('2021-08-01 14:00', '%Y-%m-%d %H:%M')
-DAY2STOP = datetime.strptime('2021-08-01 20:00', '%Y-%m-%d %H:%M')
+DAY1START = datetime.strptime('2022-04-02 14:00', '%Y-%m-%d %H:%M')
+DAY1STOP = datetime.strptime('2022-04-03 04:00', '%Y-%m-%d %H:%M')
+DAY2START = datetime.strptime('2022-04-03 14:00', '%Y-%m-%d %H:%M')
+DAY2STOP = datetime.strptime('2022-04-03 20:00', '%Y-%m-%d %H:%M')
 
