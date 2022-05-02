4ade2c459aa89b1ea5a77a015366b21f1e47c8dd
n0so <n0so@arrl.net>
2022-05-01 23:11:33 -0500

    updates to moqplogfile and fixoldloggers

--- moqputils/fixoldloggers.py
+++ moqputils/fixoldloggers.py
@@ -9,41 +9,59 @@ Update History:
 -V0.0.1 - Just getting started
 """
 
-from moqputils.moqpqsoutils import MOQPQSOUtils
+from moqputils.moqplogfile import MOQPLogFile
 
-class fixOldLoggers(MOQPQSOUtils):
+class fixOldLoggers(MOQPLogFile):
 
     BADLOGGERS = ['WRITELOG V11']
 
-    def __init__(self, wlData=None, wlFile=None):
-        if (wlFile):
-            logtext = self.readFile(wlFile,linesplit=False)
-            if (logtext):
-                if (self.IsThisACabFile(logtext)):
-                    wlData = logtext
-                else:
-                    print('File %s is not a CABRILLO file.'\
-                           %(wlFile))
-            else:
-                print('Could not read file %s'%(wlFile))
-        if(wlData):
-           self.newFileData=self.fixFileData(wlData)
-        else:
-           self.newFileData=None
-           
+    """ 
+    Over ride method initLog to add room for the updated
+    log file (FIXEDLOG)
+    """
+    def initLog(self):
+        #Initialize RAWLOG, HEADER and QSOLIST
+        MOQPLogFile.initLog(self) 
+        """Add FIXEDLOG - should hold new log with 
+        extra fields removed."""
+        self.FIXEDLOG = None 
+    """ 
+    Two new methods:
+    fixQSO - fixes a single QSO line removing the extra
+             sequence fields,
+    
+    fixQSOList - Removes the extra fields from all qsos in a 
+                 list of QSOs by calling fixQSO for each record.  
+                 
+    isLoggerObsolete - Takes one parameter: The string from the 
+                       log header CREATED-BY: tag. the string
+                       will be compared to known obsolete versions
+                       of logging software that still insert seq.
+                       numbers in QSO field# 7 and 11 and return
+                       True if the string matches, or False if not.
+                      
+    """          
     def fixQSO(self, qso):
         newQSO = None
+        qso = qso.upper()
+        #Split off the 'QSO:' text
+        tagpos = qso.find('QSO:')
+        qsoData=self.packLine(qso[tagpos+4:])
+        """
         linesplit = qso.split(':')
         lineparts = len(linesplit)
         #print('%d Split data items: %s, %s'%(lineparts, linesplit[0],linesplit[1].strip()))
         qsoTag=linesplit[0].strip()
         qsoData = linesplit[1].strip()
+        """
         temp = qsoData.replace(':','')
         qsoParts = temp.split(' ')
         qsoLen = len(qsoParts)
         
+        """7th [6] and 11th [10] fields are seq numbers in
+        obsolete log - skip them."""
         if (qsoLen >= 12):
-            newQSO = '%s %s %s %s %s %s %s %s %s %s'%(\
+            newQSO = 'QSO: %s %s %s %s %s %s %s %s %s %s'%(\
                                                       qsoParts[0],
                                                       qsoParts[1],
                                                       qsoParts[2],
@@ -64,31 +82,83 @@ class fixOldLoggers(MOQPQSOUtils):
             newList.append(self.fixQSO(qso))
         return newList
         
-
-    def fixFileData(self, fileData):
-        retData = None
-        log = self.getLogdictData(fileData)
-        logApp = log['HEADER']['CREATED-BY'].strip()
+    def isLoggerObsolete(self, createdBy):
+        loggerIsObsolete = False
+        logApp = createdBy.strip().upper()
         for v in self.BADLOGGERS:
             if (v in logApp):
-               newQSOList = self.fixQSOList(log['QSOLIST'])
+               loggerIsObsolete = True
                break
-        #Copy header to new string
-        retData = []
-        hData = fileData.splitlines()
-        hIndex = 0
-        addNote = True
-        while not(hData[hIndex].startswith('QSO:')):
-            if addNote:
-                if 'SOAPBOX:' in hData[hIndex]:
-                    retData.append('SOAPBOX: fixoldloggers removed obsolete seq. numbers from QSO list.')
-                    addNote=False
-            retData.append(hData[hIndex])
-            hIndex += 1
-        #Copy new QSO list
-        for qso in newQSOList:
-            retData.append('QSO:' + qso)
-        retData.append('END-OF-LOG:')
-        #print(newQSOList)
-        return retData
+        return loggerIsObsolete
+    
+
+    """
+    Over ride method buildLog() to insert (or tack on)
+    the code to detect and remove extra fields.
+    """
+    def buildLog(self, Log=None, Persist = True):
+        logF = None # Returned if errors happen!
+        """
+        Call the parent and let it create the data
+        with errors. Make sure to set Persist = True
+        so the RAWLOG and HEADER variables get populated.
+        QSOLIST will be as well, bit it will be replaced
+        with our data if sequence fields exist in the log
+        """
+        log=MOQPLogFile.buildLog(self, Log, Persist=True)
+        """
+        print('RAWLOG={}\n\nHEADER={}\n\nQSOLIST={}'.format(\
+          self.RAWLOG,
+          log['HEADER'], 
+          log['QSOLIST']))"""
         
+        """Now we have the log - was it created by one of the
+        old logging programs we know about?"""        
+        if (self.isLoggerObsolete(log['HEADER']['CREATED-BY'])):
+            #Yes, obsolete - remove seq fields if they exist.
+            #print('Obsolete logger detected!')
+            #Get a fresh copy of the QSO list
+            #print('RAWLOG = '.format(self.RAWLOG))
+            loglist = self.getLogList(self.RAWLOG)
+            tlog = self.getLogdictData(self.RAWLOG)
+            """Remove extra fields from QSO list and
+               build list of dict indexed qsos."""
+            fixedQList = self.fixQSOList(tlog['QSOLIST'])
+            fixedDictList = self.buildQSOList(fixedQList)
+
+            """
+            Populate FIXEDLOG as a LIST of lines (no dict 
+            objects) representing the adjusted log file.
+            """
+            self.FIXEDLOG = self.buildFixedList(loglist, 
+                                                 fixedQList)
+            """
+            Build new log to return
+            """
+            logF = dict()
+            logF['HEADER'] = log['HEADER']
+            if ('SOAPBOX' in logF['HEADER']):
+                logF['HEADER']['SOAPBOX'] += \
+                'Obsolte Sequence numbers removed by fixoldloggers.py.'
+            logF['QSOLIST'] = fixedDictList
+        else:
+            #Nothing to do, return log provided by parent.
+            logF = log
+            
+        return logF
+        
+    def buildFixedList(self, header, newqsos):
+        newLogList = []
+
+        hIndex = 0
+        while not(header[hIndex].strip().upper().startswith('QSO:')):
+            newLogList.append(header[hIndex])
+            hIndex += 1
+
+        newLogList.append(\
+        'SOAPBOX: Obsolte Sequence numbers removed by fixoldloggers.py.')
+        
+        for line in newqsos:
+            newLogList.append(line)
+            
+        return newLogList
---
+++ moqputils/moqplogfile.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+"""
+MOQPLogFile     - An MOQP Log File structure with processing
+                  utilities.
+                
+Update History:
+* Thu Apr 28 2022 Mike Heitmann, N0SO <n0so@arrl.net>
+- V0.0.1 - Just getting started
+* Sun May 01 2022 Mike Heitmann, N0SO <n0so@arrl.net>
+- V1.0.0 - Ready for first release.
+-          Added method sortQSOdictList to sort the resulting QSO
+-          list in various ways. Default is by DATETIME in ascending
+-          order. 
+"""
+import os
+from moqputils.moqpqsoutils import MOQPQSOUtils
+
+
+
+class MOQPLogFile(MOQPQSOUtils):
+    QSOTAGS = ['FREQ', 'MODE', 'DATETIME', 'MYCALL',
+               'MYREPORT', 'MYQTH', 'URCALL', 'URREPORT', 'URQTH', 'NOTES']
+
+    def __init__(self, logFile=None):
+        self.initLog()
+        if (logFile):
+            if (isinstance(logFile,str)):
+                if (os.path.isfile(logFile)):
+                    self.buildLog(logFile)
+            
+    """
+    Initialize log object:
+       Set interal values RAWLOG, HEADER, QSOLIST to None
+    """
+    def initLog(self):
+        self.RAWLOG = None
+        self.HEADER = None
+        self.QSOLIST = None
+        
+    """
+    Fetch data from raw log file and return as a single 
+    string of text if Perist is set True the raw log text
+    string will also be save in self.RAWLOG
+    """
+    def readLogText(self, logpath, Persist = False):
+       
+       rawlog = self.readFile(logpath, linesplit = False)
+       #print(isinstance(rawlog,str),'\n',rawlog)
+       if (self.IsThisACabFile(rawlog.strip())):
+          if (Persist):
+             self.RAWLOG = rawlog
+       else:
+          rawlog = None
+       return rawlog
+       
+    """
+    Return the logfile data as a list of lines.
+    if a logString is passed, it is parsed and
+    returned as a list. If no logstring is passed, the
+    string in self.RAWLOG is parsed returned as a list.
+    """
+    def getLogList(self, logString=None):
+       logList = None
+       if (logString == None):
+           logString = self.RAWLOG
+       
+       if (logString):
+           logList = []
+           logList = logString.splitlines()
+       
+       return logList
+   
+    """
+    Build the MOQP Log object:
+       if the input  parameter Log is a list, it is assumed
+       to be a the raw log as a list of lines.
+       
+       if the parameter Log is a string, is is assumed to be
+       the path to a file containing the raw log data.
+       
+       If the parameter Log is set to None or omitted, the
+       raw log text will be taken from self.RAWLOG if it is
+       something other than None type.
+       
+       The header and qso list will be returned as a dict
+       object (log in the example below) with two elements:
+        log['HEADER']  - A dict object of the log header, with 
+                         the dictionary index elements using the 
+                         names defined in self.CABRILLOTAGS
+        log['QSOLIST'] - A list of dictionary objects, with each
+                         element in the list a dictionary of a QSO
+                         record, with the dictionary index elements
+                         using the names defined in self.QSOTAGS.
+                
+       Set Persist = False if it is not desired to save the header
+       data in self.HEADER and the qso list saved as self.QSOLIST.
+    """
+    def buildLog(self, Log=None, Persist = True):
+       logList = None
+       if(Log):
+           if(isinstance(Log, str)):
+               #Path name string, read file for data
+               logText = self.readLogText(Log, Persist)
+               logList = self.getLogList(logText)
+           elif (isinstance(Log, list)):
+               #List of raw log file text lines
+               logList = Log
+       else:
+           logList=self.getLogList(self.RAWLOG)
+    
+       if (logList):
+           log = self.getLogdictData(logText)
+           #print(log)
+           dictqsoList = self.buildQSOList(log['QSOLIST'])
+           log['QSOLIST'] = dictqsoList
+           if (Persist):
+               self.HEADER = log['HEADER']
+               self.QSOLIST = dictqsoList               
+           return log
+       else:
+           return None
+    """
+    sortQSOdictList - Sort a list of qso record dictionary objects.
+      This uses the python sort() function and is taylored for 
+      sorting MOQP QSO lists of dict objesct Default action is 
+      to by the DATETIME key in ascending order.
+
+    qsodictList - A list object containing QSO records in dict
+                  format where each element in the object is
+                  indexed by the key names defined in QSOTAGS.
+                  If omitted, the internal object sel.QSOLIST
+                  will be sorted and returned.
+                  
+    sortKey     - String containing the dict key name to sort 
+                  by. The default key is DATETIME. 
+                  This may be omitted if the default action of
+                  sort by DATETIME is desired.
+                  
+    sortLtoS    - A boolean representing the sort order desired.
+                  True = largest to smallest, False = smallest 
+                  to largest. Defaults to smallest to largest.
+                  This my be omitted if the default behavior of
+                  smallest to largest is desired.
+                  
+    
+    """       
+    def sortQSOdictList(self, qsodictList=None, 
+                              sortKey='DATETIME',
+                              sortLtoS = False):
+           newList = None
+           if qsodictList == None:
+                  qsodictList = self.QSOLIST
+           newList = sorted(qsodictList, 
+                            key=lambda i: i[sortKey],
+                            reverse = sortLtoS)
+           return newList       
+           
+    def buildQSOList(self, qsolist):
+       qsodictList = None
+       if (qsolist and isinstance(qsolist, list)):
+           qsodictList = []
+           for qso in qsolist:
+               #Split off the 'QSO:' text
+               tagpos = qso.find('QSO:')
+               tqso=self.packLine(qso[tagpos+4:])
+               #print(tqso)
+               dictqso = self.getQSOdict(tqso)
+               qsodictList.append(dictqso)               
+       return qsodictList
+    
+    def showQSO(self, qso):
+      textqso = ('QSO: {} {} {} {} {} {} {} {} {}'.format(\
+                     qso['FREQ'],
+                     qso['MODE'],
+                     qso['DATETIME'],
+                     qso['MYCALL'],
+                     qso['MYREPORT'],
+                     qso['MYQTH'], 
+                     qso['URCALL'],
+                     qso['URREPORT'], 
+                     qso['URQTH']))
+      return textqso     
+
+    def showQSOList(self, qsolist=None):
+      if qsolist == None:
+        qsolist = self.QSOLIST
+      textqsolist =''
+      for qso in qsolist:        
+        textqsolist += '{}\n'.format(self.showQSO(qso))
+      return textqsolist
+
+    def showHeader(self, header = None, tagList = None):
+      if header == None:
+        #Use oject HEADER
+        header = self.HEADER
+      if tagList == None:
+        tagList = self.CABRILLOTAGS
+
+      textheader = ''
+      for tag in tagList:
+        #if not( tag in ['QSO','END-OF-LOG']):
+        textheader += '{}: {}\n'.format(tag, header[tag])
+        
+      return textheader
+      
