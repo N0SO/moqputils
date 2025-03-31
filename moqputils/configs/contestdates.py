1a7ca6cab5ecbe1b2f31b2a84a6a1bf35d4cbc11
n0so <n0so@arrl.org>
2025-03-30 22:06:23 -0500

    Updating contest dates for 2025

--- moqputils/configs/contestdates.py
+++ moqputils/configs/contestdates.py
@@ -4,10 +4,10 @@
      contest start/stop times.
 """
 from datetime import datetime
-DAY1START = datetime.strptime('2024-04-06 14:00', '%Y-%m-%d %H:%M')
-DAY1STOP = datetime.strptime('2024-04-07 04:00', '%Y-%m-%d %H:%M')
-DAY2START = datetime.strptime('2024-04-07 14:00', '%Y-%m-%d %H:%M')
-DAY2STOP = datetime.strptime('2024-04-07 20:00', '%Y-%m-%d %H:%M')
+DAY1START = datetime.strptime('2025-04-05 14:00', '%Y-%m-%d %H:%M')
+DAY1STOP = datetime.strptime('2025-04-06 04:00', '%Y-%m-%d %H:%M')
+DAY2START = datetime.strptime('2025-04-06 14:00', '%Y-%m-%d %H:%M')
+DAY2STOP = datetime.strptime('2025-04-06 20:00', '%Y-%m-%d %H:%M')
 """
 print('Using contest dates:\n{} - {}\n{} - {}'.format(DAY1START,
                                                       DAY1STOP,
