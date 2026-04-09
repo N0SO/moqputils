fc4d7fd54776f83b89ecef1c606ca3ffc19cd9be
n0so <n0so@arrl.org>
2026-04-08 20:56:13 -0500

    2025 config final

--- bin/fetchlogs.sh
+++ bin/fetchlogs.sh
@@ -2,9 +2,10 @@
 # Fetch the latest MOQP logs from W0MA
 # Brute force, needs generalization.
 #
-cd /home/pi/Public/moqplogs/2025
+YEAR=2026
+cd /home/pi/Public/moqplogs/$YEAR
 # Redirect all output to log file
 exec >> file-downloadlog.txt 2>&1
 echo "---------> start of download: "; date
-rsync --stats -av  w0ma@w0ma.org:/home/w0ma/mo_qso_party/results/2025/logs/ ./logfiles/from_w0ma
+rsync --stats -av  w0ma@w0ma.org:/home/w0ma/mo_qso_party/results/$YEAR/logs/ ./logfiles/from_w0ma
 
